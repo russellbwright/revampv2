4aea3f79f8fda2947ba28810361c7a4934bf0ee6
Russell Wright <russell.bwright@yahoo.com>
2017-10-11 01:06:22 -0500

    changing the singleProjectService

--- public/src/components/singleProject/singleProjectCtrl.js
+++ public/src/components/singleProject/singleProjectCtrl.js
@@ -3,20 +3,20 @@ angular.module('myApp').controller('singleProjectCtrl', function($scope, project
 
 $scope.test = "imbad"
 
-$scope.singleProject = (id) => {
+// $scope.singleProject = (id) => {
     
 
-    projectService.singleProject(id).then(response => {
-        console.log(response.data[0])
-        return $scope.proj = response.data[0];
+//     projectService.singleProject(id).then(response => {
+//         console.log(response.data[0])
+//         return $scope.proj = response.data[0];
     
-    })
+//     })
     
-   }
+//    }
    
-//    projectService.singleProject().then(result => {
-//        console.log(result.data)
-//        $scope.proj = result.data[0];
-//    })
+   projectService.singleProject().then(result => {
+       console.log(result.data)
+       $scope.proj = result.data[0];
+   })
 
 })
