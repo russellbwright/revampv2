b45a118facde61b154bb30191fc5743ade402380
Russell Wright <russell.bwright@yahoo.com>
2017-10-13 16:24:30 -0500

    sessions work need to add key when making new project

--- public/src/components/projectList/projectService.js
+++ public/src/components/projectList/projectService.js
@@ -4,7 +4,7 @@ angular.module('myApp').service('projectService', function($http, $q){
 
 
     this.addProject = function(project){
-        // console.log(project)
+        console.log(project)
         return $http.post('/api/projects/createProject', [project.last, project.first, project.company, project.units, project.shortDesc, project.type])
     }, 
 
