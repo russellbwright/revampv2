e7d8f36207f9948453f16c372ee9857e99ce2dac
Russell Wright <russell.bwright@yahoo.com>
2017-10-16 00:41:36 -0500

    logo is commented out and a better signup is commented out

--- public/src/components/register/registerCtrl.js
+++ public/src/components/register/registerCtrl.js
@@ -13,5 +13,24 @@ angular
       
   };
 
+
+//   $scope.Signup = function(name, email, pass1, pass2) {
+//     console.log('running signup')
+//     var newUser = {
+//       "name": name,
+//       "email": email,
+//       "password": pass1
+//     }
+//     console.log(newUser)
+//     if(pass1 !== pass2) {
+//       window.alert('Passwords do not match')
+//     } else {
+//       signupService.createUser(newUser).then(function(result) {
+//           console.log('signupControlelr', result)
+//       });
+//     }
+//   }
+// });
+
   
 });
