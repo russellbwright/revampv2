5570630993f2f2aea60106ed914f9c76e8cbed43
Russell Wright <russell.bwright@yahoo.com>
2017-10-17 00:05:25 -0500

    finished front page, cleaned up other data

---
+++ public/footerDir.js
@@ -0,0 +1,12 @@
+angular.module('myApp').directive('footerDir', function(){
+    
+            return {
+                templateUrl: './src/components/footerDir/footerDir.html',
+                // restrict: 'E',
+                // scope:{
+    
+                // }
+    
+            }
+    
+    })
