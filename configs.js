1ee0b0a6befc71a519a9de8a650c2d9e9c519cbe
Russell Wright <russell.bwright@yahoo.com>
2017-11-13 18:47:51 -0600

    config stuff, hosted working

--- configs.js
+++ configs.js
@@ -6,7 +6,7 @@ module.exports = {
         address: "postgres://scvcufkjhdxfgv:5990cf15c8cec4a552fad7837fcc6223f8c5769a27615d3a0c76d950b9d41b16@ec2-54-235-250-15.compute-1.amazonaws.com:5432/d1onp08fd6dfr6?ssl=true"
         },
         
-        session: {
+     session: {
             secret: 'supersecret',
     
         },
